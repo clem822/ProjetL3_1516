8127fd4bfc4dde952999228d562154c0df9ee142
yaakoub <c.yak32000@gmail.com>
2016-01-05 03:47:57 +0100

    modif mineur

--- src/serveur/element/Guerrier.java
+++ src/serveur/element/Guerrier.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Guerrier extends Personnage {
 
 	public Guerrier(String nom, String groupe, HashMap<Caracteristique, Integer> caracts) {
-		super(nom, groupe, caracts);
+		super("Geurrier", groupe, caracts);
 		// TODO Auto-generated constructor stub
 	}
 	
